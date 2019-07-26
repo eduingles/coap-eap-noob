ef36ae39d68b272274135807715d0b5dad432da1
Aleksi Peltonen <aleksi.peltonen@aalto.fi>
2019-07-26 14:30:48 +0300

    Moved database implementation to separate file

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ UIP_CONF_IPV6=1
 #MODULES += os/services/shell # Enable only in case you need to use the shell
 MODULES += os/lib/json # For Contiki-NG to include JSON library
 
-PROJECT_SOURCEFILES = eap-noob.c eap-peer.c ecc_pubkey.c ecc_shared_secret.c eax.c aes.c _cantcoap.c
+PROJECT_SOURCEFILES = eap-noob.c eap-peer.c database.c ecc_pubkey.c ecc_shared_secret.c eax.c aes.c _cantcoap.c
 # eap-psk.c
 
 # Configure PANID based on user: 1 Eduardo
--- eap-noob.c
+++ eap-noob.c
@@ -253,7 +253,7 @@ static int json_integer_value(struct jsonparse_state *js, const char *key)
 /**
  * initMethodEap : Initialise EAP method
 **/
-void init_eap_noob()
+void init_eap_noob(void)
 {
     // Set default NAI
     sprintf(nai, "%s%s", "noob@", DEFAULT_REALM);
@@ -265,81 +265,6 @@ void init_eap_noob()
 **/
 void eap_noob_build_identity(char *eapRespData) {
     memcpy(eapRespData, nai, strlen(nai)+1);
-}
-
-/**
- * write_db : Write data to database
- * @key : key
- * @val : value
- * Returns : 1 or -1
-**/
-static int write_db(char *key , char *val)
-{
-    int db;
-    if ((db = cfs_open(DB_NAME, CFS_WRITE | CFS_APPEND)) >= 0) {
-        cfs_write(db, key, strlen(key));
-        cfs_write(db, ";", 1);
-        cfs_write(db, val, strlen(val));
-        cfs_write(db, "\n", 1);
-        cfs_close(db);
-    } else {
-        DEBUG_NOOB("Could not open database");
-        return -1;
-    }
-    return 1;
-}
-
-/**
- * read_db : Read data from database
- * @key : key
- * @val : value
- * Returns : 1 or -1
-**/
-static int read_db(char *key, char *val)
-{
-    int db;
-    if ((db = cfs_open(DB_NAME, CFS_READ)) >= 0) {
-        // Read the entire database
-        size_t size = cfs_seek(db, 0, CFS_SEEK_END);
-        cfs_seek(db, 0, CFS_SEEK_SET);
-        char dst[size];
-        cfs_read(db, dst, size);
-        cfs_close(db);
-        // Find value in database
-    	char *current_line = strtok(dst, "\n");
-    	while(current_line != NULL) {
-            char *current_key = strtok(NULL, ";");
-            char *current_val = strtok(NULL, "\n");
-            if (!strcmp(current_key, key)) {
-                memcpy(val, current_val, strlen(current_val) + 1);
-                return 1;
-            }
-    	}
-        return -1;
-    } else {
-        DEBUG_NOOB("Could not open database");
-        return -1;
-    }
-}
-
-/**
- * print_db : Print database to stdout
- * TEMPORARY - FOR DEBUGGING PURPOSES
-**/
-static void print_db()
-{
-    int db;
-    if ((db = cfs_open(DB_NAME, CFS_READ)) >= 0) {
-        size_t size = cfs_seek(db, 0, CFS_SEEK_END);
-        cfs_seek(db, 0, CFS_SEEK_SET);
-        char dst[size];
-        cfs_read(db, dst, size);
-        cfs_close(db);
-        printf("Database after parsing request \n%s\n", dst);
-    } else {
-        DEBUG_NOOB("Could not open database");
-        return -1;
-    }
 }
 
 /**
@@ -379,7 +304,7 @@ void generate_nonce(size_t size, unsigned char *dst)
 /**
  * generate_noob : Generate fresh 16 byte nonce and save it to the database
  **/
-void generate_noob()
+void generate_noob(void)
 {
     unsigned char noob[23];
     generate_nonce(16, noob);
